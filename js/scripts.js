f5bed9abbee4b05072148bc6748d1cc5137c2d20
marcinosuch <mar.osuch@gmail.com>
2017-05-27 14:17:51 +0200

    funkcja

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+// scripts.js
+
+function getTriangleArea(a,h) {
+var a/* = prompt('Enter base of triangle')*/;
+	h/* = prompt('Enter height of triangle')*/;
+if (a <= 0 || h <= 0){
+	alert('Nieprawidłowe dane');
+	console.log('Nieprawidłowe dane');}
+/*else {triangleArea=a*h/2;
+	console.log(triangleArea);
+	alert(triangleArea);}*/
+
+return (a*h/2)
+
+}
+console.log( getTriangleArea(10,6) )
+
+
+var triangle1Area = getTriangleArea(10, 15);
+	triangle2Area = getTriangleArea(1, 2);
+	triangle3Area = getTriangleArea(5, 50);
+console.log( triangle1Area )
+console.log( triangle2Area )
+console.log( triangle3Area )
+/*getTriangleArea()*/
+
+
+	/*
+
+
+if (value > 0) {
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+} else {
+	console.log('zero');}
+
+
+var triangleArea = a * h / 2
+
+
+
+
+var a = prompt('Enter base of triangle');
+	h = prompt('Enter height of triangle');
+	triangleArea = a * h / 2
+	tA = alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+/*
+var a = prompt('Enter a');
+	b = prompt('Enter b');
+	value = (a * a) + (2 * a * b) - (b * b)
+	tA = alert('(a * a) + (2 * a * b) - (b * b) =  ' + value);
+	console.log('(a * a) + (2 * a * b) - (b * b) =  ' + value);
+
+if (value > 0) {
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+} else {
+	console.log('zero');}
+*/
+/*
+
+var selectedTab = 'details';
+
+if (selectedTab == 'details') {
+    console.log('You selected details tab');
+} else if (selectedTab == 'comments') {
+    console.log('You selected comments tab');
+} else {
+    console.log('You selected product description tab');
+}
+
+*/
+
+
+/*
+
+	name = prompt('Enter your name');
+	alert('Hello, ' + name);
+	console.log('Hello, ' + name);
+
+*/
+
+
+
